34d4f8823bbe8e05735804cbbed5f7c36462b9c7
Lior Rubin <lior.rubin@post.runi.ac.il>
2025-10-29 22:56:58 +0200

    Finished First exercise

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,18 @@
 // Splits a restaurant bill evenly among three diners.
-public class Bill3 {
-	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+// The program will get 3 names and the dinner price
+// This program will print the value each person will have to pay rounden up
+public class Bill3 
+{
+	public static void main(String[] args) 
+	{
+	    String firstName = args[0];
+		String secondName = args[1];
+		String thirdName = args[2];
+		int dinnerPrice = Integer.parseInt(args[3]); // each args is a string value, Integer.parseInt makes it be a int value
+
+		double priceEach = (double)dinnerPrice / 3; // dinnerPrice is a int value so changing it to double so we can round up later
+		priceEach = Math.ceil(priceEach); // rounding up the price
+
+		System.out.println("Dear " + thirdName + ", " + secondName + ", and " + firstName + ": pay " + priceEach + " Shekels each.");
 	}
 }
