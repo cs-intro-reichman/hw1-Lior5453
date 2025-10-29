2f0b9f17a9763e0e9880d88e298d0bb9545ce4d5
Lior Rubin <lior.rubin@post.runi.ac.il>
2025-10-29 23:38:58 +0200

    Finished third exercise

--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,11 @@
 // Prints a given number using a hundreds, tens, and units notation.
-public class NumWords {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
+// This function gets 1 number and prints its value with houndreads, tens and ones
+public class NumWords 
+{
+	public static void main(String args[]) 
+	{
+		int number = Integer.parseInt(args[0]);
+
+		System.out.println(number/100 + " hundreds, " + number/10%10 + " tens, and " + number%10 + " ones.");
 	}
 }
